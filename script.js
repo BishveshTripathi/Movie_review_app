321bee88b2d20c0591692d2389a815d4c3d9a702
Bishvesh Tripathi <rajtripathi54671@gmail.com>
2023-09-28 12:13:08 +0530

    more customized css

--- script.js
+++ script.js
@@ -16,20 +16,23 @@ const showMovieData = (md) => {
   movies.innerHTML = "";
   const { Title, Genre, imdbRating, Plot, Poster, Released, Runtime } = md;
 
-  const movieElement = document.createElement("div");
-  movieElement.classList.add("movie_element");
-  movieElement.innerHTML = `<h1>${Title}</h1><p><strong>Rating : ${imdbRating}</strong></p>`;
-  movies.appendChild(movieElement);
-
-  const movieItems = document.createElement("div");
-  movieItems.classList.add("movie_items");
-  movieItems.innerHTML = `<h1>${Genre}</h1><p><strong>Plot : ${Plot}</strong></p>`;
-  movies.appendChild(movieItems);
-
   const moviePoster = document.createElement("div");
   moviePoster.classList.add("movie_poster");
-  moviePoster.innerHTML = <img src="${Poster}"></img>;
+  moviePoster.innerHTML = `<img src="${Poster}"/>`;
   movies.appendChild(moviePoster);
+
+  const movieElement = document.createElement("div");
+  movieElement.classList.add("movie_element");
+  movieElement.innerHTML = `<h1>${Title}</h1>
+  <h2><strong>Rating : ${imdbRating}</strong></h2>
+  <h2>${Genre}</h2>
+  <h2><strong>Storyline: ${Plot}</strong></h2>`;
+  movies.appendChild(movieElement);
+
+  // const movieItems = document.createElement("div");
+  // movieItems.classList.add("movie_items");
+  // movieItems.innerHTML = `<h1>${Genre}</h1><p><strong>Plot : ${Plot}</strong></p>`;
+  // movies.appendChild(movieItems);
 };
 
 searchform.addEventListener("submit", (e) => {
